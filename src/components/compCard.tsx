e281e6eacd8ba343bca72b8504e9530065dbcdcf
rafifArka <rafifdwiarka123@gmail.com>
2025-08-10 03:24:17 +0700

    add new page and responsive

--- src/components/compCard.tsx
+++ src/components/compCard.tsx
@@ -4,7 +4,7 @@ const CompCard = () => {
 
     return (
         <div className="h-64 w-[65vw] bg-white border-2 border-yellow shadow-2xl px-20 flex items-center gap-12 rounded-lg">
-            <img src="" alt="tes tes" className="flex bg-amber-300 w-56 h-56 shrink-0"/>
+            <img src="src/assets/logo/sriwijaya.png" alt="tes tes" className="flex w-56 h-56 shrink-0"/>
             <div className="flex flex-col h-56 justify-between py-2">
                 <div className="flex flex-col pr-60 gap-2">
                     <div className="font-bebas text-h2 text-red leading-none">Sriwijawa international taekwondo championship 2025</div>
