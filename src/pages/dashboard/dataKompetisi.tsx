8433899244b238872c1376e8a48f8357268a3a03
Dwii <rafifdwiarka123@gmail.com>
2025-09-14 01:30:24 +0700

    fix inii

--- src/pages/dashboard/dataKompetisi.tsx
+++ src/pages/dashboard/dataKompetisi.tsx
@@ -913,13 +913,6 @@ const handleCloseDeleteModal = () => {
             <Trash size={18} />
           </button>
         </div>
-
-        <AlertModal
-          isOpen={deleteModal.isOpen}
-          onClose={handleCloseDeleteModal}
-          onConfirm={handleConfirmDelete}
-          message={`Apakah Anda yakin ingin menghapus peserta "${deleteModal.participantName}" dari kompetisi ini?`}
-        />
       </div>
     );
   })}
@@ -1013,6 +1006,14 @@ const handleCloseDeleteModal = () => {
             </div>
           </>
         )}
+
+                <AlertModal
+            isOpen={deleteModal.isOpen}
+            onClose={handleCloseDeleteModal}
+            onConfirm={handleConfirmDelete}
+            message={`Apakah Anda yakin ingin menghapus peserta "${deleteModal.participantName}" dari kompetisi ini?`}
+          />
+
       </div>
 
       
