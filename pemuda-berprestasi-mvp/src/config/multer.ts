e00cefb363a9cc8ebbe0fccb65814c77615b1dc0
Duwiii0 <rafifdwiarka123@gmail.com>
2025-09-28 21:50:31 +0700

    ubah format timestamp tidak bisa (/)

--- pemuda-berprestasi-mvp/src/config/multer.ts
+++ pemuda-berprestasi-mvp/src/config/multer.ts
@@ -20,7 +20,7 @@ const getTimestamp = () => {
   const hh = String(now.getHours()).padStart(2, '0')
   const min = String(now.getMinutes()).padStart(2, '0')
 
-  return `${dd}/${mm}/${yyyy}_${hh}.${min}`
+  return `${dd}-${mm}-${yyyy}_${hh}.${min}`
 }
 
 
