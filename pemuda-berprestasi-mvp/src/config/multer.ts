a4320c4cd648dcd69803b5053bf077483bd42b19
Duwiii0 <rafifdwiarka123@gmail.com>
2025-09-28 21:45:26 +0700

    ubah datenow agar terbaca

--- pemuda-berprestasi-mvp/src/config/multer.ts
+++ pemuda-berprestasi-mvp/src/config/multer.ts
@@ -12,6 +12,18 @@ const getUploadsPath = () => {
   return path.join(process.cwd(), 'uploads');
 }
 
+const getTimestamp = () => {
+  const now = new Date()
+  const dd = String(now.getDate()).padStart(2, '0')
+  const mm = String(now.getMonth() + 1).padStart(2, '0')
+  const yyyy = now.getFullYear()
+  const hh = String(now.getHours()).padStart(2, '0')
+  const min = String(now.getMinutes()).padStart(2, '0')
+
+  return `${dd}-${mm}-${yyyy}_${hh}-${min}`
+}
+
+
 const createUploadDirs = () => {
   const baseDir = getUploadsPath();
   const dirs = [
@@ -70,7 +82,7 @@ const storage = multer.diskStorage({
   filename: async (req, file, cb) => {
   const user = req.user as any
   const type = file.fieldname
-  const timestamp = Date.now()
+  const timestamp = getTimestamp()
   const ext = path.extname(file.originalname)
   let filename = ''
 
