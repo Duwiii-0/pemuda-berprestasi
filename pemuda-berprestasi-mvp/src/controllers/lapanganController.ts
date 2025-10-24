f1fe7690b2ffcb1bd390804906979ef9c958c9c4
Duwiii0 <rafifdwiarka123@gmail.com>
2025-10-24 07:51:49 +0700

    fix : import lapangan service

--- pemuda-berprestasi-mvp/src/controllers/lapanganController.ts
+++ pemuda-berprestasi-mvp/src/controllers/lapanganController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import lapanganService from "../services/lapangan.service";
+import lapanganService from "../services/lapanganService";
 
 export class LapanganController {
   async tambahHariLapangan(req: Request, res: Response) {
