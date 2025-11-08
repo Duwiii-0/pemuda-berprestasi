67b4726dc36af9457822a83f38cbead0ae2229bb
Elmanda1 <falihelmanda@gmail.com>
2025-11-08 16:52:31 +0700

    feat(utils): updated layout & design for pdf (151) [DEBUG]

--- src/utils/exportBracketPDF.ts
+++ src/utils/exportBracketPDF.ts
@@ -464,10 +464,10 @@ if (isPrestasi) {
   console.log('📏 PRESTASI zoom:', zoom);
 } else {
   // ✅ Zoom diperkecil untuk PEMULA (zoom out)
-  if (totalPeserta <= 8) zoom = 1.2;
-  else if (totalPeserta <= 16) zoom = 1.1;
-  else if (totalPeserta <= 32) zoom = 0.9;
-  else zoom = 0.8;
+  if (totalPeserta <= 8) zoom = 1.15;
+  else if (totalPeserta <= 16) zoom = 1.05;
+  else if (totalPeserta <= 32) zoom = 1;
+  else zoom = 0.9;
   console.log('📏 PEMULA zoom:', zoom);
 }
 
