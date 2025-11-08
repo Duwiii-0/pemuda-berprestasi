7cfdf5f085b0c84ded66283f0d3a65795b968b03
Duwiii0 <rafifdwiarka123@gmail.com>
2025-11-08 21:48:25 +0700

    fix: perbeasar nama

--- pemuda-berprestasi-mvp/src/controllers/lapanganController.ts
+++ pemuda-berprestasi-mvp/src/controllers/lapanganController.ts
@@ -135,7 +135,6 @@ export class LapanganController {
 
   async getHariLapanganByKompetisi(req: Request, res: Response) {
     try {
-      console.log("<<<<< TESTING NODEMON: Lapangan controller is being hit >>>>>");
       const { id_kompetisi } = req.params;
 
       if (!id_kompetisi) {
