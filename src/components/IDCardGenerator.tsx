e71b5675b7196bcf8a28e34514feff8591edb96c
Elmanda1 <falihelmanda@gmail.com>
2025-11-08 21:53:17 +0700

    feat(ui): E-ID Card for atlet (23)

--- src/components/IDCardGenerator.tsx
+++ src/components/IDCardGenerator.tsx
@@ -24,7 +24,7 @@ const OVERLAY_COORDS = {
   // Photo box (kotak besar kiri) - koordinat FIXED berdasarkan template
   photo: {
     x: 25.5,           // Posisi X foto
-    y: 98,           // Posisi Y foto (dari atas)
+    y: 95,           // Posisi Y foto (dari atas)
     width: 77,       // Lebar foto FIXED
     height: 108,      // Tinggi foto FIXED
   },
