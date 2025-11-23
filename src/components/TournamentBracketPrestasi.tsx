d478ad833232e114d0aad0ec8af67e18262233b2
Elmanda1 <falihelmanda@gmail.com>
2025-11-23 08:59:28 +0700

    fix (push): perbaikan bagan untuk atlet 3 peserta (5)

--- src/components/TournamentBracketPrestasi.tsx
+++ src/components/TournamentBracketPrestasi.tsx
@@ -2977,7 +2977,7 @@ approvedParticipants.length === 3 ? (
         alignSelf: "flex-start", // ⭐ Align ke atas
         marginTop: `${55 + CARD_HEIGHT / 2}px`, // ⭐ Header (55px) + setengah card
         height: "2px",
-        width: `${CENTER_GAP + 50}px`,
+        width: `${CENTER_GAP + 100}px`,
       }}
     >
       <svg
